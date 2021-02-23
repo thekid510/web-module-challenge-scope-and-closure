4f30347fced405d6eb060469d28fa0e0bd355019
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-02-22 16:08:16 -0800

    working on final task

--- index.js
+++ index.js
@@ -96,15 +96,23 @@ function finalScore(callback, number){
   Away = Away + callback();
 
 }
-return{Home , Away};
+return{Home, Away};
 }
+console.log(finalScore(inning,9));
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(cb,number) {
+  let Home = 0;
+  let Away = 0;
+  for(let i=0; i <= number; i++){
+  Home = Home + callback();
+  Away = Away + callback();
+
+}
+return{Home, Away};
 }
 
 
